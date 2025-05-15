60fe001696ef793b85adca872facce81bd61c113
menettibomminn <diegomenetti@gmail.com>
2025-05-16 00:44:52 +0200

    Codice per l'attacco - da completare
    Ho messo giù le prime righe di codice per l'attacco, bisogna definire bene dove e come statistiche e avversari.

--- Assets/CFX/Troops/PlayerMove.cs
+++ Assets/CFX/Troops/PlayerMove.cs
@@ -9,19 +9,36 @@ public class PlayerMove : MonoBehaviour
 
     public bool isInTurn;
     public bool isSelected;
+    public bool isMoving;
+    public bool isAttacking;
 
     private Vector3 offsetY = new Vector3(0, 2, 0);
+
+    public int idTroop;
+
+    private Vector3 rangeWarrior = new Vector3(1.1f, 1.1f, 1.1f);
+    private Vector3 rangeWizard = new Vector3(5.1f, 5.1f, 5.1f);
+    private Vector3 rangeArcher = new Vector3(7.1f, 7.1f, 7.1f);
 
     private void Awake()
     {
         isSelected = true;
+        isMoving = true;
+        isAttacking = true;
     }
 
     private void Update()
     {
         if (isSelected)
         {
-            CameraRayCastMovement();
+            if (isMoving)
+            {
+                CameraRayCastMovement();
+            }
+            else if (isAttacking) 
+            {
+                CameraRayCastAttack();
+            }
         }
     }
 
@@ -52,6 +69,84 @@ public class PlayerMove : MonoBehaviour
                 return;
             }
         }
+    }
+
+    public void CameraRayCastAttack()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        RaycastHit targetHit;
+        int layerMaskGridnode = LayerMask.GetMask("GridNode");
+        int layerMaskCharacter = LayerMask.GetMask("Character");
+        GameObject target = new GameObject();
+
+        switch (idTroop)
+        {
+            case 0:
+                WarriorAttackUpdate();
+                break;
+            case 1:
+                ArcherAttackUpdate();
+                break;
+            case 2:
+                WizardAttackUpdate();
+                break;
+        }
+
+        void WarriorAttackUpdate() 
+        {
+            if (Physics.Raycast(ray, out hit, 1000f, layerMaskCharacter) && Input.GetMouseButtonDown(0))
+            {
+
+                target = hit.transform.gameObject;
+
+                targetHit = hit;
+
+                if (GetDistanceRayCast(targetHit, gameObject).x < rangeWarrior.x && GetDistanceRayCast(targetHit, gameObject).z < rangeWarrior.z)
+                {
+
+                }
+            }
+        }
+
+        void ArcherAttackUpdate()
+        {
+            if (Physics.Raycast(ray, out hit, 1000f, layerMaskCharacter) && Input.GetMouseButtonDown(0))
+            {
+
+                target = hit.transform.gameObject;
+
+                targetHit = hit;
+
+                if (GetDistanceRayCast(targetHit, gameObject).x < rangeArcher.x && GetDistanceRayCast(targetHit, gameObject).z < rangeArcher.z)
+                {
+
+                }
+            }
+        }
+
+        void WizardAttackUpdate()
+        {
+            if (Physics.Raycast(ray, out hit, 1000f, layerMaskGridnode) && Input.GetMouseButtonDown(0))
+            {
+
+                target = hit.transform.gameObject;
+
+                targetHit = hit;
+
+                if (GetDistanceRayCast(targetHit, gameObject).x < rangeWizard.x && GetDistanceRayCast(targetHit, gameObject).z < rangeWizard.z)
+                {
+
+                }
+            }
+        }
+    }
+
+    public Vector3 GetDistanceRayCast(RaycastHit hit, GameObject troop)
+    {
+        Vector3 distance = new Vector3();
+        distance = hit.transform.position - troop.transform.position;
+        return distance;
     }
 
     public GameObject OnGridNode()
